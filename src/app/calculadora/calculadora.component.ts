1c729c2c1b4d6e129e62001e34c03c47492eeda8
wesleyfelix <wesley.felixdelima96@hotmail.com>
2025-07-16 21:46:07 -0300

    usando condição na redenrizaçã da tela

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
